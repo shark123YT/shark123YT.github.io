14b2e77887da976da5a1fbe4e0348babf2b1e101
shark123YT <137716913+shark123YT@users.noreply.github.com>
2023-06-25 19:25:23 +0200

    Delete TESTmod.js

--- TESTmod.js
+++
@@ -1,9 +0,0 @@
-// Adding elements:
-elements.TEST1 = {
-    color: "#133738",
-    behavior: behaviors.LIQUID,
-    category: "liquids",
-    viscosity: 100000,
-    state: "liquid",
-    density: 720,
-};
